5608a4f55792442c943a4303a27088177b3c8d54
Your Name <you@example.com>
2022-12-23 20:08:48 +0300

    23/12/22 end version + vercel

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,17 @@
 import express, {Request, Response} from "express";
 import {videosRouter} from "./routse/videos-router";
 import {db} from "./routse/videos-router";
-export const app=express();
 
-const convertJson=express.json();
+export const app = express();
+
+const convertJson = express.json();
 app.use(convertJson);
 
-app.use('/videos',videosRouter);
+app.use('/videos', videosRouter);
 
 
-app.delete('/testing/all-data',(req:Request,res:Response)=>{
-db.splice(0,db.length);
-
-res.status(204)
+app.delete('/testing/all-data', (req: Request, res: Response) => {
+    db.splice(0, db.length);
+    res.sendStatus(204)
 });
 
