6613b18cc1ce3ec8585f46c930131c0993650390
Your Name <you@example.com>
2023-01-25 23:03:46 +0300

    25/01/23 mongodb new branch.

--- src/db.ts
+++ src/db.ts
@@ -1,12 +1,13 @@
 const {MongoClient} =require('mongodb');
-//process.env.mongoURI ||'mongodb://localhost:27017/baseOne';
-const mongoURI='mongodb://127.0.0.1:27017';
+
+const mongoURI=process.env.mongoURI ||'mongodb://127.0.0.1:27017';
 //mongodb+srv://adminincubator:<password>@clusterapi.o5rglql.mongodb.net/?retryWrites=true&w=majority
+
 export const client = new MongoClient(mongoURI)
 export async function runDB(){
     try{
         await client.connect();
-        //await client.db('baseOne').command({ping:1})
+        //await client.db('hometask3').command({ping:1})
     console.log('Connect successful')
     }
     catch (e) {
