2fe531c3ebe995c625055f27f7b96077fdbbac65
Your Name <you@example.com>
2023-01-25 23:28:04 +0300

    25/01/23 mongodb new branch.

--- src/db.ts
+++ src/db.ts
@@ -1,13 +1,12 @@
 const {MongoClient} =require('mongodb');
 
 const mongoURI=process.env.mongoURI ||'mongodb://127.0.0.1:27017';
-//mongodb+srv://adminincubator:<password>@clusterapi.o5rglql.mongodb.net/?retryWrites=true&w=majority
 
 export const client = new MongoClient(mongoURI)
 export async function runDB(){
     try{
         await client.connect();
-        //await client.db('hometask3').command({ping:1})
+        await client.db('hometask3').command({ping:1})
     console.log('Connect successful')
     }
     catch (e) {
