e386d69099af1d235cb01abf4096191670bcd646
Your Name <you@example.com>
2023-03-10 23:23:26 +0300

    09/03/23 test.

--- src/routse/commentsRouter.ts
+++ src/routse/commentsRouter.ts
@@ -9,6 +9,8 @@ import {postCommentForPostValidation} from "../Models/InputValidation";
 import {blogsCollection, commentsCollection} from "../db";
 import {authMiddleware} from "../Middleware/authMiddleware";
 import {createSecureServer} from "http2";
+import {commentsService} from "./commentsService";
+import {authCommentUser} from "../Middleware/authCommentUser";
 
 export const commentsRouter=Router({})
 
@@ -43,12 +45,14 @@ commentsRouter.get('/:id',async (req:Request,res:Response)=>{
 })
 
 //401 and 403 not execute
-commentsRouter.put('/:id',authMiddleware,postCommentForPostValidation,async (req:Request,res:Response)=>{
-    console.log(req.params.id)
+commentsRouter.put('/:id',authMiddleware,authCommentUser,postCommentForPostValidation,async (req:Request,res:Response)=>{
 
     const pagination=getPaginationUpdateComment(req.params,req.body)
+    //const checkCommentsByUser=await commentsService.checkCommentsUser(req.user!.id,pagination.id)
+    //if(!checkCommentsByUser)
     const resultCommentUpdate=await updateComment(pagination.id,pagination.content)
     console.log(resultCommentUpdate)
+
     if(!resultCommentUpdate){
         return res.sendStatus(404)
     }
@@ -58,10 +62,11 @@ commentsRouter.put('/:id',authMiddleware,postCommentForPostValidation,async (req
 commentsRouter.delete('/:commentId',authMiddleware,async (req:Request,res:Response)=>{
     const pagination=getPaginationDeleteCommentById(req.params)
     const resultSearch=await getCommentOnId(pagination.id)
-    console.log(resultSearch)
     if(!resultSearch){
         return res.sendStatus(404)
     }
     await commentsCollection.deleteOne({id: pagination.id})
     return res.sendStatus(204);
 })
+
+//в каждом методе if который требует все комменты,если с ревеста к
--- src/routse/commentsService.ts
+++ src/routse/commentsService.ts
@@ -1,5 +1,14 @@
-import {InputCommentByIdType} from "../RepositoryInDB/commentator-repositoryDB";
+import {getCommentOnId, InputCommentByIdType} from "../RepositoryInDB/commentator-repositoryDB";
 
 export const commentsService={
-
+ async checkCommentsUser(userId:string,commentId:string){
+  const resultSearch=await getCommentOnId(commentId)
+  if(!resultSearch){
+   return false;
+  }
+  if(resultSearch.commentatorInfo.userId!==userId){
+   return false;
+  }
+  return true;
+ }
 }
