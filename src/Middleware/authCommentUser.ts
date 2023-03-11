e5ee86856b67473e060480c41748d8080acc7549
Your Name <you@example.com>
2023-03-11 11:53:24 +0300

    09/03/23 test.

--- src/Middleware/authCommentUser.ts
+++ src/Middleware/authCommentUser.ts
@@ -4,11 +4,11 @@ import {getCommentOnId} from "../RepositoryInDB/commentator-repositoryDB";
 
 export const authCommentUser =async (req: Request, res: Response, next: NextFunction) => {
     const resultSearch=await getCommentOnId(req.params.id)
-    console.log(resultSearch)
+
     if(!resultSearch){
         return res.sendStatus(404)
     }
-    console.log(req.user?.id)
+
     if(resultSearch.commentatorInfo.userId!==req.user?.id){
         return res.sendStatus(403)
     }
--- src/routse/commentsService.ts
+++ src/routse/commentsService.ts
@@ -1,14 +1,5 @@
 import {getCommentOnId, InputCommentByIdType} from "../RepositoryInDB/commentator-repositoryDB";
 
 export const commentsService={
- async checkCommentsUser(userId:string,commentId:string){
-  const resultSearch=await getCommentOnId(commentId)
-  if(!resultSearch){
-   return false;
-  }
-  if(resultSearch.commentatorInfo.userId!==userId){
-   return false;
-  }
-  return true;
- }
+
 }
