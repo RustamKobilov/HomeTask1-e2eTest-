b5c7db33c5a63a13b6bf1ae39683bb2cd1b578ca
Your Name <you@example.com>
2023-03-11 12:09:30 +0300

    11/03/23 fix code.

--- src/domain/authService.ts
+++ src/domain/authService.ts
@@ -3,17 +3,13 @@ import bcrypt from "bcrypt";
 import {jwtService} from "../application/jwtService";
 
 export const authService = {
-
-    //  async authUser():Promise<UserType>{
-    //      const user = await userRepository.findUserByLoginOrEmail(loginOrEmail)
-    //      if(!user){return false}
-    //      const resultUCompareDateUser=bcrypt.compare(password, user.hash)
-    //      if(!resultUCompareDateUser){return false}
-    // },
     async login(loginOrEmail: string, password: string): Promise<false|UserType> {
         const user = await userRepository.findUserByLoginOrEmail(loginOrEmail)
         if(!user){return false}
+        console.log(user.hash)
+        console.log(password)
         const resultCompare=bcrypt.compare(password, user.hash)
+        console.log(resultCompare)
         if(!resultCompare){return false}
     return user
     }
