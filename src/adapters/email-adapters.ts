1cbacaa5228025419098a01c9d77daaca63ea79e
Your Name <you@example.com>
2023-03-18 15:27:57 +0300

    18/03/23 email send.

--- src/adapters/email-adapters.ts
+++ src/adapters/email-adapters.ts
@@ -15,8 +15,11 @@ export const emailAdapters={
             from:'admin <rustamincubator@gmail.com>',
             to:email,
             subject:'Registration in platform',
-            html: 'https://somesite.com/confirm-email?code='+code
-        //"</h1>"+code+"<h1>"
+            html: ' <h1>Thank for your registration</h1>\n' +
+                '       <p>To finish registration please follow the link below:\n' +
+                '          <a href=\'https://somesite.com/confirm-email?code='+code+'\'>complete registration</a>\n' +
+                '      </p>'
+
 
         })
     }
