d07e8e9df6d749a08180b64c3b0846d98eb5d7d5
Your Name <you@example.com>
2023-04-24 23:52:10 +0300

    tests 24.04.23

--- src/test/apiTest.test.ts
+++ src/test/apiTest.test.ts
@@ -725,9 +725,12 @@ describe('Session and device for User /SecurityDevices', ()=> {
     let refreshTokenCookies:any=null;
     let userId:any=null;
     let deviceId:any=null;
+    let lastActiveDate:any=null;
     let deviceName='TestDevice1'
+    let deviceName2='TestDevice2'
+    let deviceName3='TestDevice3'
 
-    it('ffffvf',async ()=>{
+    it('add device,during auth/login true',async ()=>{
         const CreateUserResponse = await request(app).post('/users/').set(BasicAuthorized.authorization, BasicAuthorized.password).send(userForChecking1).expect(201)
 
         const AuthUserResponse = await request(app).post('/auth/login').set('User-Agent',deviceName).send(userAuthForChecking1).expect(200)
@@ -742,26 +745,79 @@ describe('Session and device for User /SecurityDevices', ()=> {
         refreshToken= refreshTokenCookies.map(function (val:string){return val.split('=')[1]}).map(function(val:string){return val.split(';')[0]})[0]
 
         expect(refreshToken).toEqual(expect.any(String))
-        ///
-        userId=CreateUserResponse.body.id
-        accessToken=AuthUserResponse.body.accessToken
-        ///
 
         const securityDeviceResponse=await request(app).get('/security/devices')
             .set('Cookie', [refreshTokenCookies])
             .expect(200)
 
+        lastActiveDate=await jwtService.getLastActiveDateFromRefreshToken(refreshToken)
+        const verifyToken=await jwtService.verifyToken(refreshToken)
+        deviceId=verifyToken.deviceId
+
         expect(securityDeviceResponse.body).toEqual([{
             ip: expect.any(String),
             title: deviceName,
-            lastActiveDate: expect.any(String),
-            deviceId: expect.any(String)
+            lastActiveDate: lastActiveDate,
+            deviceId: deviceId
         }])
-        console.log(securityDeviceResponse.body)
+    })
+    it('add many devices for one user',async ()=>{
+        const AddDevice2Response = await request(app).post('/auth/login').set('User-Agent',deviceName2).send(userAuthForChecking1).expect(200)
+        const AddDevice3Response = await request(app).post('/auth/login').set('User-Agent',deviceName3).send(userAuthForChecking1).expect(200)
 
-        //['user-agent']
+        //refresh2
+        const cookies2 = AddDevice2Response.headers['set-cookie']
+        const refreshTokenCookies2=cookies2.filter(function(val:string){return val.split('=')[0]=='refreshToken'})
+        const refreshToken2= refreshTokenCookies2.map(function (val:string){return val.split('=')[1]}).map(function(val:string){return val.split(';')[0]})[0]
+        expect(refreshToken2).toEqual(expect.any(String))
+        //
+
+        //refresh3
+        const cookies3 = AddDevice3Response.headers['set-cookie']
+        const refreshTokenCookies3=cookies3.filter(function(val:string){return val.split('=')[0]=='refreshToken'})
+        const refreshToken3= refreshTokenCookies3.map(function (val:string){return val.split('=')[1]}).map(function(val:string){return val.split(';')[0]})[0]
+        expect(refreshToken).toEqual(expect.any(String))
+        //
+
+        //lastActiveDate and deviceId2
+        const lastActiveDate2=await jwtService.getLastActiveDateFromRefreshToken(refreshToken2)
+        const verifyToken2=await jwtService.verifyToken(refreshToken2)
+        const deviceId2=verifyToken2.deviceId
+        //
+
+        //lastActiveDate and deviceId3
+        const lastActiveDate3=await jwtService.getLastActiveDateFromRefreshToken(refreshToken3)
+        const verifyToken3=await jwtService.verifyToken(refreshToken3)
+        const deviceId3=verifyToken3.deviceId
+        //
+
+        const securityDeviceResponse=await request(app).get('/security/devices')
+            .set('Cookie', [refreshTokenCookies3])
+            .expect(200)
+
+        expect(securityDeviceResponse.body).toEqual([{
+            ip: expect.any(String),
+            title: deviceName,
+            lastActiveDate: lastActiveDate,
+            deviceId: deviceId
+        },
+            {
+                ip: expect.any(String),
+                title: deviceName2,
+                lastActiveDate: lastActiveDate2,
+                deviceId: deviceId2
+            },
+            {
+                ip: expect.any(String),
+                title: deviceName3,
+                lastActiveDate: lastActiveDate3,
+                deviceId: deviceId3
+            }
+
+        ])
 
     })
+
 })
 
 
