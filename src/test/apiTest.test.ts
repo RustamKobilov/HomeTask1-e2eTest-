5cbb51dd8413d72c31344759c0f30424cc4fac10
Your Name <you@example.com>
2023-04-26 16:47:44 +0300

    tests 26.04.23

--- src/test/apiTest.test.ts
+++ src/test/apiTest.test.ts
@@ -8,7 +8,14 @@ import {header} from "express-validator";
 import {authRouter} from "../routse/authRouter";
 import {sessionsTypeCollection} from "../db";
 import {jwtService} from "../application/jwtService";
-import {throws} from "assert";
+import {notEqual, throws} from "assert";
+
+const delay= async(ms:number)=>{
+    return new Promise<void>((resolve,reject)=>{
+      setTimeout(()=>resolve(),ms)
+    })
+}
+
 
 
 const BasicAuthorized={
@@ -705,7 +712,7 @@ describe('auth/registration-confirmation test',  ()=> {
 })
 
 describe('Session and device for User /SecurityDevices', ()=> {
-
+    jest.setTimeout(60000)
     beforeAll(async () => {
         await request(app).delete('/testing/all-data')
     })
@@ -715,44 +722,54 @@ describe('Session and device for User /SecurityDevices', ()=> {
         password: '1234222223',
         email: 'tryToken1@ram.by'
     }
-    const userAuthForChecking1={
-        loginOrEmail:userForChecking1.login,
-        password:userForChecking1.password
+    const userAuthForChecking1 = {
+        loginOrEmail: userForChecking1.login,
+        password: userForChecking1.password
     }
 
-    let accessToken:any=null;
-    let refreshToken:any=null;
-    let refreshTokenCookies:any=null;
-    let userId:any=null;
-    let deviceId:any=null;
-    let lastActiveDate:any=null;
-    let deviceName='TestDevice1'
-    let deviceName2='TestDevice2'
-    let deviceName3='TestDevice3'
+    let accessToken: any = null;
+    let refreshToken: any = null;
+    let refreshTokenCookies: any = null;
+    let userId: any = null;
+    let deviceId: any = null;
+    let deviceId2: any = null;
+    let deviceId3: any = null;
+    let lastActiveDate: any = null;
+    let lastActiveDate2: any = null;
+    let lastActiveDate3: any = null;
+    let deviceName = 'TestDevice1'
+    let deviceName2 = 'TestDevice2'
+    let deviceName3 = 'TestDevice3'
 
-    it('add device,during auth/login true',async ()=>{
+    it('add device,during auth/login true', async () => {
         const CreateUserResponse = await request(app).post('/users/').set(BasicAuthorized.authorization, BasicAuthorized.password).send(userForChecking1).expect(201)
 
-        const AuthUserResponse = await request(app).post('/auth/login').set('User-Agent',deviceName).send(userAuthForChecking1).expect(200)
+        const AuthUserResponse = await request(app).post('/auth/login').set('User-Agent', deviceName).send(userAuthForChecking1).expect(200)
 
         expect(AuthUserResponse.body.accessToken).toEqual(expect.any(String))
 
         ///refreshToken
         const cookies = AuthUserResponse.headers['set-cookie']
         console.log(cookies)
-        refreshTokenCookies=cookies.filter(function(val:string){return val.split('=')[0]=='refreshToken'})
+        refreshTokenCookies = cookies.filter(function (val: string) {
+            return val.split('=')[0] == 'refreshToken'
+        })
         console.log('refresh token cookies ' + refreshTokenCookies)
-        refreshToken= refreshTokenCookies.map(function (val:string){return val.split('=')[1]}).map(function(val:string){return val.split(';')[0]})[0]
+        refreshToken = refreshTokenCookies.map(function (val: string) {
+            return val.split('=')[1]
+        }).map(function (val: string) {
+            return val.split(';')[0]
+        })[0]
 
         expect(refreshToken).toEqual(expect.any(String))
 
-        const securityDeviceResponse=await request(app).get('/security/devices')
+        const securityDeviceResponse = await request(app).get('/security/devices')
             .set('Cookie', [refreshTokenCookies])
             .expect(200)
 
-        lastActiveDate=await jwtService.getLastActiveDateFromRefreshToken(refreshToken)
-        const verifyToken=await jwtService.verifyToken(refreshToken)
-        deviceId=verifyToken.deviceId
+        lastActiveDate = await jwtService.getLastActiveDateFromRefreshToken(refreshToken)
+        const verifyToken = await jwtService.verifyToken(refreshToken)
+        deviceId = verifyToken.deviceId
 
         expect(securityDeviceResponse.body).toEqual([{
             ip: expect.any(String),
@@ -761,39 +778,53 @@ describe('Session and device for User /SecurityDevices', ()=> {
             deviceId: deviceId
         }])
     })
-    it('add many devices for one user',async ()=>{
-        const AddDevice2Response = await request(app).post('/auth/login').set('User-Agent',deviceName2).send(userAuthForChecking1).expect(200)
-        const AddDevice3Response = await request(app).post('/auth/login').set('User-Agent',deviceName3).send(userAuthForChecking1).expect(200)
+    it('add many devices for one user', async () => {
+        const AddDevice2Response = await request(app).post('/auth/login').set('User-Agent', deviceName2).send(userAuthForChecking1).expect(200)
+        const AddDevice3Response = await request(app).post('/auth/login').set('User-Agent', deviceName3).send(userAuthForChecking1).expect(200)
 
         //refresh2
         const cookies2 = AddDevice2Response.headers['set-cookie']
-        const refreshTokenCookies2=cookies2.filter(function(val:string){return val.split('=')[0]=='refreshToken'})
-        const refreshToken2= refreshTokenCookies2.map(function (val:string){return val.split('=')[1]}).map(function(val:string){return val.split(';')[0]})[0]
+        const refreshTokenCookies2 = cookies2.filter(function (val: string) {
+            return val.split('=')[0] == 'refreshToken'
+        })
+        const refreshToken2 = refreshTokenCookies2.map(function (val: string) {
+            return val.split('=')[1]
+        }).map(function (val: string) {
+            return val.split(';')[0]
+        })[0]
         expect(refreshToken2).toEqual(expect.any(String))
         //
 
         //refresh3
         const cookies3 = AddDevice3Response.headers['set-cookie']
-        const refreshTokenCookies3=cookies3.filter(function(val:string){return val.split('=')[0]=='refreshToken'})
-        const refreshToken3= refreshTokenCookies3.map(function (val:string){return val.split('=')[1]}).map(function(val:string){return val.split(';')[0]})[0]
+        const refreshTokenCookies3 = cookies3.filter(function (val: string) {
+            return val.split('=')[0] == 'refreshToken'
+        })
+        const refreshToken3 = refreshTokenCookies3.map(function (val: string) {
+            return val.split('=')[1]
+        }).map(function (val: string) {
+            return val.split(';')[0]
+        })[0]
         expect(refreshToken).toEqual(expect.any(String))
         //
 
         //lastActiveDate and deviceId2
-        const lastActiveDate2=await jwtService.getLastActiveDateFromRefreshToken(refreshToken2)
-        const verifyToken2=await jwtService.verifyToken(refreshToken2)
-        const deviceId2=verifyToken2.deviceId
+        lastActiveDate2 = await jwtService.getLastActiveDateFromRefreshToken(refreshToken2)
+        const verifyToken2 = await jwtService.verifyToken(refreshToken2)
+        deviceId2 = verifyToken2.deviceId
         //
 
         //lastActiveDate and deviceId3
-        const lastActiveDate3=await jwtService.getLastActiveDateFromRefreshToken(refreshToken3)
-        const verifyToken3=await jwtService.verifyToken(refreshToken3)
-        const deviceId3=verifyToken3.deviceId
+        lastActiveDate3 = await jwtService.getLastActiveDateFromRefreshToken(refreshToken3)
+        const verifyToken3 = await jwtService.verifyToken(refreshToken3)
+        deviceId3 = verifyToken3.deviceId
         //
 
-        const securityDeviceResponse=await request(app).get('/security/devices')
+        const securityDeviceResponse = await request(app).get('/security/devices')
             .set('Cookie', [refreshTokenCookies3])
             .expect(200)
+
+        console.log(securityDeviceResponse.body)
 
         expect(securityDeviceResponse.body).toEqual([{
             ip: expect.any(String),
@@ -817,7 +848,87 @@ describe('Session and device for User /SecurityDevices', ()=> {
         ])
 
     })
+    it('input device,before created. refresh device', async () => {
+        const OldDeviceInputResponse = await request(app).post('/auth/login').set('User-Agent', deviceName3).send(userAuthForChecking1).expect(200)
 
+        accessToken = OldDeviceInputResponse.body.accessToken
+        refreshTokenCookies = OldDeviceInputResponse.headers['set-cookie']
+        refreshToken = refreshTokenCookies.filter(function (val: string) {
+            return val.split('=')[0] == 'refreshToken'
+        })//refreshTokenUpdate
+
+        const securityDeviceResponse = await request(app).get('/security/devices')
+            .set('Cookie', [refreshTokenCookies])
+            .expect(200)
+
+        const deviceRefreshName3 = securityDeviceResponse.body.filter(function (val: any) {
+            return val.title == deviceName3
+        })
+        console.log(securityDeviceResponse.body)
+        expect(securityDeviceResponse.body.length).toEqual(3)
+        expect(securityDeviceResponse.body[0]).toEqual({
+            ip: expect.any(String),
+            title: deviceName,
+            lastActiveDate: lastActiveDate,
+            deviceId: deviceId
+        })
+        expect(securityDeviceResponse.body[1]).toEqual({
+            ip: expect.any(String),
+            title: deviceName2,
+            lastActiveDate: lastActiveDate2,
+            deviceId: deviceId2
+        })
+        expect(deviceRefreshName3.deviceId).not.toEqual(deviceId3)
+        expect(deviceRefreshName3.lastActiveDate).not.toEqual(lastActiveDate3)
+
+    })
+
+    it('check update refreshToken,expire deviceId', async () => {
+        await delay(10000)
+        const NewDeviceInputResponse = await request(app).post('/auth/login')
+            .set('User-Agent', deviceName3)
+            .send(userAuthForChecking1).expect(200)
+
+        const newAccessToken = NewDeviceInputResponse.body.accessToken
+        const newRefreshTokenCookies = NewDeviceInputResponse.headers['set-cookie']
+        const newRefreshToken = newRefreshTokenCookies.filter(function (val: string) {
+            return val.split('=')[0] == 'refreshToken'
+        })//refreshTokenUpdate
+        const AuthUserRefreshTokensResponse = await request(app).post('/auth/refresh-token')
+            .set({Authorization: 'bearer ' + newAccessToken})
+            .set('Cookie', [refreshTokenCookies]).expect(401)
+
+    })
+})
+    describe('limit request for one Ip',  ()=> {
+
+        beforeAll(async () => {
+            await request(app).delete('/testing/all-data')
+        })
+
+        const userForChecking1 = {
+            login: 'token1',
+            password: '1234222223',
+            email: 'tryToken1@ram.by'
+        }
+        const userAuthForChecking1 = {
+            loginOrEmail: userForChecking1.login,
+            password: userForChecking1.password
+        }
+
+        let deviceName = 'TestDevice1'
+
+        it('check limit request for one device',async ()=>{
+            const CreateUserResponse = await request(app).post('/users/').set(BasicAuthorized.authorization, BasicAuthorized.password).send(userForChecking1).expect(201)
+        for(let x=0;x<4;x++){
+            const AuthUserResponse1 = await request(app).post('/auth/login').set('User-Agent',deviceName).send(userAuthForChecking1)
+            const AuthUserResponseFalseLoginMin = await request(app).post('/auth/registration').send(userForChecking1)
+            console.log(x+1)
+        }
+            const AuthUserResponse1 = await request(app).post('/auth/login').set('User-Agent',deviceName).send(userAuthForChecking1).expect(429)
+            const AuthUserResponseFalseLoginMin = await request(app).post('/auth/registration').send(userForChecking1).expect(429)
+
+    })
 })
 
 
