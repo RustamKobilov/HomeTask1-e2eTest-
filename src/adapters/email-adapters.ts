469baf2433eb19cea2eb1777402892740b75c91d
Your Name <you@example.com>
2023-05-07 16:39:38 +0300

    realize new password,but no time(html text) 06.05.23

--- src/adapters/email-adapters.ts
+++ src/adapters/email-adapters.ts
@@ -37,11 +37,12 @@ export const emailAdapters={
                 from:'admin <rustamincubator@gmail.com>',
                 to:email,
                 subject:'Password recovery in platform',
-            html: '<h1>Password recovery</h1>'+
-            '<p>To finish password recovery please follow the link below:'+
-                ' <a href=\'https://somesite.com/password-recovery?recoveryCode=' + recoveryCode + '>recovery password</a>'+
-             '</p>'
+            html: ' <h1>Password recovery\n' +
+                '       <p>To finish password recovery please follow the link below:\n' +
+                '          <a href=\'https://somesite.com/password-recovery?recoveryCode='+recoveryCode+'\'>recovery password</a>\n' +
+                '      </p>'
 
-    })
+
+        })
     }
 }
